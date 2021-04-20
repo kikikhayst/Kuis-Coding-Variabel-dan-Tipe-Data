cd2872d9bc56c1f6cde6e1a4eb66e0e43c0b3aaf
kikikhayst <73214485+kikikhayst@users.noreply.github.com>
2021-04-20 14:49:42 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+/**
+ * TODO:
+ * Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+ *  - firstName: bertipe data string, dengan nilai nama depan Anda.
+ *  - lastName: bertipe data string, dengan nilai nama belakang Anda.
+ *  - age: bertipe data number, dengan nilai umur Anda.
+ *  - isMarried: bertipe data boolean, dengan bebas Anda tentukan.
+ */
+
+ let firstName = "Rizqi Rama";
+ let lastName = "Hayustya";
+ let age = 21;
+ let isMarried = false;
+ 
+ console.log(firstName);
+ console.log(lastName);
+ console.log(age);
+ console.log(isMarried);
+ 
+ /**
+  * Hiraukan kode di bawah ini
+  */
+ 
+ module.exports = {
+   firstName, lastName, age, isMarried,
+ };
+ 
